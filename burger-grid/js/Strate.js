850967280fddb9896029862c43fb0c3e41012c10
baptistegodart <113678515+baptistegodart@users.noreply.github.com>
2022-12-12 16:08:57 +0100

    update burger, made animated strate on click

--- burger-grid/js/Strate.js
+++ burger-grid/js/Strate.js
@@ -1,10 +1,25 @@
-class Strate{
+const sStates = {
+    FALLING:1,
+    DROPED:2
+}
+
+const sTypes = {
+    TOP_BREAD:1,
+    BOTTOM_BREAD: 2,
+    CHEESE:3,
+    SALAD:4,
+    STEAK:5,
+    TOMATO:6
+}
+
+class Strate {
     constructor(i, type) {
         this.x = 0
         this.y = 0
         this.width = 0
         this.height = 0
 
+        this.xOffset = random(-30, 30)
         this.corners = {
             topLeft : 5,
             topRight : 5,
@@ -12,70 +27,83 @@ class Strate{
             bottomLeft: 5
         }
 
-        this.types = {
-            TOP_BREAD:1,
-            BOTTOM_BREAD: 2,
-            CHEESE:3,
-            SALAD:4,
-            STEAK:5,
-            TOMATO:6
-        }
-
+        this.currState = sStates.FALLING;
         this.type = this.selectStrateType(type, i)
 
         switch(this.type){
-            case this.types.TOP_BREAD:
+            case sTypes.TOP_BREAD:
+
                 this.color = 'rgb(232, 198, 139)'
+
+                this.xOffset = 0
                 this.corners.topLeft = roundCornerValue
                 this.corners.topRight = roundCornerValue
+
             break;
-            case this.types.BOTTOM_BREAD:
+
+            case sTypes.BOTTOM_BREAD:
+
                 this.color = 'rgb(232, 198, 139)'
+
+                this.xOffset = 0
                 this.corners.bottomLeft = roundCornerValue
                 this.corners.bottomRight = roundCornerValue
+
             break;
-            case this.types.CHEESE:
+
+            case sTypes.CHEESE:
                 this.color = 'rgb(255, 231, 113)'
             break;
-            case this.types.SALAD:
+
+            case sTypes.SALAD:
                 this.color = 'rgb(135, 223, 117)'       
             break;
-            case this.types.STEAK:
+
+            case sTypes.STEAK:
                 this.color = 'rgb(134, 68, 45)'
             break;
-            case this.types.TOMATO:
+
+            case sTypes.TOMATO:
                 this.color = 'rgb(233, 47, 59)'
             break;
         }
+
+        this.easingPosY = new Easing({
+            duration: 500,
+            from: -height/nStrates,
+            easing: EASINGS.easeInCubic
+        })
+
     }
 
     draw(x, y, w, h) {
 
-        this.x = x
+        this.x = x + this.xOffset
         this.y = y
         this.width = w
         this.height = h
 
         push()
+        stroke(0)
         fill(this.color)
         rect(this.x, this.y, this.width, this.height, this.corners.topLeft, this.corners.topRight, this.corners.bottomRight, this.corners.bottomLeft)
         pop()
         
     }
 
-    selectStrateType(type, index){
+    selectStrateType(type, index) {
 
         let selectedType
 
         switch(type){
 
             case bread:
-                const selectedBread = index == 0 ? this.types.TOP_BREAD : this.types.BOTTOM_BREAD
+                const selectedBread = index == nStrates-1 ? sTypes.TOP_BREAD : sTypes.BOTTOM_BREAD
                 selectedType = selectedBread // this.types.BREAD
             break;
 
             case garniture:
-                const typesLength = Object.keys(this.types).length
+                const typesLength = Object.keys(sTypes).length
                 const rdnGarniture = Math.round(random(3, typesLength))
 
                 selectedType = rdnGarniture
@@ -85,5 +113,7 @@ class Strate{
 
         return selectedType
     }
+
+    
 
 }
--- burger-grid/main.js
+++ burger-grid/main.js
@@ -4,10 +4,13 @@ let credit = `Code by :
 www.baptistegodart.ch`
 
 let nStrates = 6 // ! Include top & bottom bun
+let widthRatio = 2 // Strates width = width/widthRatio
 
 let strates = []
+let strateCounter = 0
 
-let roundCornerValue = 100;
+let roundCornerValue = 100
+
 let bread = 'bread'
 let garniture = 'garniture'
 
@@ -16,13 +19,15 @@ function showCredits() {
 }
 
 function setup() {
+    
+    showCredits()
     createCanvas(windowWidth, windowHeight)
+
     angleMode(DEGREES)
     rectMode(CENTER)
-    showCredits()
 
-    initStrate()
-    console.log(strates)
+    // initStrate()
+
 }
 
 function windowResized() {
@@ -33,34 +38,40 @@ function draw() {
 
     background(255)
     noStroke()
+    
+    translate(width/(2*widthRatio), height/nStrates/2)
+    // drawGrid()
 
-    translate(width/2.4, height/nStrates/2)
+    for(let i=0; i<strateCounter; i++){
+    // for(let i=0; i<nStrates; i++){
 
-    for(let i=0; i<nStrates; i++){
-
-        const strateWidth = width/1.2
+        const easingValue = strates[i].easingPosY.update(deltaTime)
+        
+        const strateWidth = width/widthRatio
         const strateHeight = height/nStrates
-
+        
         const posX = width/2 - strateWidth/2
-        const posY = i*strateHeight
+        // const posY = height - ((i + 1) * strateHeight)
+        const posY = strates[i].currState == sStates.FALLING ? easingValue : height - (i+1)*strateHeight
 
         drawBurger(posX, posY, strateWidth, strateHeight, i)
-        // drawGrid(posX, posY, strateWidth, strateHeight)
 
     }
 
 }
 
-function drawGrid(x, y, w, h) {
+function drawGrid() {
 
     for(let i=0; i<nStrates; i++){
 
-        push()
+        const strateHeight = height/nStrates
+        const posX = 0
+        const posY = i*strateHeight
 
+        push()
         stroke(0)
         noFill()
-        rect(x, y, w, h);
-
+        rect(posX, posY, width*2, strateHeight)
         pop()
 
     }
@@ -84,4 +95,38 @@ function drawBurger(x, y, w, h, index) {
 
     strates[index].draw(x, y, w, h)
  
+}
+
+function mouseClicked() {
+
+    const firstStrate = strates.length > 0 ? false : true
+    let lastStratDroped
+
+    if(firstStrate) {
+        lastStratDroped = true
+    }else{
+        lastStratDroped = strates[strateCounter-1].currState == sStates.DROPED ? true : false
+    }
+
+    
+    if(strateCounter < nStrates && lastStratDroped) {
+        
+        const strateType = strateCounter == 0 || strateCounter == nStrates-1 ? bread : garniture
+        const newStrate = new Strate(strateCounter, strateType)
+    
+        strates.push(newStrate)
+
+        const strateHeight = height/nStrates
+
+        strates[strateCounter].easingPosY.start({
+            to: height - (strateCounter+1)*strateHeight,
+        })
+
+        strates[strateCounter].easingPosY.onEnd = function () {
+            strates[strateCounter-1].currState = sStates.DROPED
+        }
+    
+        strateCounter++
+    }
+
 }
